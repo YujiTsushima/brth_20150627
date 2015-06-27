27997b7df22ce024207c605c77b97b87b40b7481
unknown <ssukemura@P0554.bjn.brycen.co.jp>
2015-06-27 15:11:29 +0900

    no message

--- haito_01.sql
+++ haito_01.sql
@@ -3,18 +3,19 @@ select
     team_id,
  if 1行目（1位）なら then
     truncate(power * 0.6,0)
- else 2行目(2位)なら then
-    truncate(power * 0.3)
- else 3行目(3位)なら then
-    truncate(power * 0.1)
+ else if 2行目(2位)なら then
+    truncate(power * 0.3,0)
+ else if 3行目(3位)なら then
+    truncate(power * 0.1,0)
  else -- それ以外（ないけど）
     power * 0
+ end if
 from 
     (
     -- 各チームの戦闘力を算出
     select
         team_id,sum(sum_val) as power
-    from member_tbl a
+    from tbl_member a
     group by team_id
     order by
      power
